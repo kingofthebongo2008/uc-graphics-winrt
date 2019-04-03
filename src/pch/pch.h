4f93833cd368fc0120513bce18a0f988f04d700a
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-04-03 05:28:14 +0300

    change header

--- src/pch/pch.h
+++ src/pch/pch.h
@@ -4,10 +4,11 @@
 
 #include <unknwn.h>
 
-#include "winrt/Windows.Foundation.h"
-#include "winrt/Windows.Graphics.Display.h"
-#include "winrt/Windows.UI.Xaml.Controls.h"
-#include "windows.ui.xaml.media.dxinterop.h"
+#include <winrt/base.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Graphics.Display.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <windows.ui.xaml.media.dxinterop.h>
 
 #include <wrl/client.h>
 
