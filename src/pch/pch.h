a0aaa564e642adc1b32517e666feccba785fc0c8
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-04-07 09:54:24 +0300

    added missing header

--- src/pch/pch.h
+++ src/pch/pch.h
@@ -6,9 +6,11 @@
 
 #include <winrt/base.h>
 #include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Graphics.Display.h>
+
 #include <winrt/Windows.UI.Xaml.Controls.h>
-#include <windows.ui.xaml.media.dxinterop.h>
+#include <Windows.UI.Xaml.Media.DXInterop.h>
 
 #include <wrl/client.h>
 
