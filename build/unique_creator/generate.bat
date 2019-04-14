5defa02eb0255ae40ded0844725a453b5049e2a1
Stefan Dyulgerov <stefan.dyulgerov@gmail.com>
2019-04-14 15:21:29 +0300

    simplified build files

---
+++ build/unique_creator/generate.bat
@@ -0,0 +1,33 @@
+@echo off
+
+echo ^<?xml version="1.0" encoding="utf-8"?^> > build_h.msbuild
+echo ^<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> build_h.msbuild
+
+msbuild_generator.exe --input ..\..\src\dev\ --mode h >> build_h.msbuild
+msbuild_generator.exe --input ..\..\src\pch\ --mode h >> build_h.msbuild
+
+echo ^</Project^> >> build_h.msbuild
+
+echo ^<?xml version="1.0" encoding="utf-8"?^> > build_cpp.msbuild
+echo ^<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> build_cpp.msbuild
+
+msbuild_generator.exe --input ..\..\src\dev\ --mode cpp >> build_cpp.msbuild
+msbuild_generator.exe --input ..\..\src\pch\ --mode cpp >> build_cpp.msbuild
+
+echo ^</Project^> >> build_cpp.msbuild
+
+
+echo ^<?xml version="1.0" encoding="utf-8"?^> > build.vcxproj.filters
+echo ^<Project DefaultTargets="Build" ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003"^> >> build.vcxproj.filters
+
+msbuild_generator.exe --type filters --input ..\..\src\dev\ --mode h >> build.vcxproj.filters
+msbuild_generator.exe --type filters --input ..\..\src\pch\ --mode h >> build.vcxproj.filters
+msbuild_generator.exe --type filters --input ..\..\src\dev\ --mode cpp >> build.vcxproj.filters
+msbuild_generator.exe --type filters --input ..\..\src\pch\ --mode cpp >> build.vcxproj.filters
+
+echo ^</Project^> >> build.vcxproj.filters
+
+
+
+
+
